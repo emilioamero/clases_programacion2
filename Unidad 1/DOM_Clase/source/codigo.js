2cc9f997a6849234cf5938143eb309fe5f2af2df
emilioamero <emilio.amero@gmail.com>
2024-05-21 18:05:25 -0300

    eventos en javascrip

--- Unidad 1/DOM_Clase/source/codigo.js
+++ Unidad 1/DOM_Clase/source/codigo.js
@@ -36,7 +36,7 @@ function setValorCampoTexto() {
 }
 
 //document.writeln("Hola");
-generarContenido();
+//generarContenido();
 function generarContenido() {
     let textosArray = [
         "Lorem ipsum dolor sit amet",
@@ -53,32 +53,87 @@ function generarContenido() {
 
     let contenedor = document.getElementById("contenedor");
 
-
-    /*    array.forEach(element => {
-   
-       });
-    */
-
     let colorAlternado = false;
-    for (let index = 0; index < textosArray.length; index++) {
+    textosArray.forEach(elementodelarray => {
         let parrafo = document.createElement('p');
 
-        parrafo.innerText = textosArray[index];
+        parrafo.innerText = elementodelarray;
         if (colorAlternado == true) {
             parrafo.className = "estilo1";
             colorAlternado = false;
-            parrafo.hidden = true;
+            // parrafo.hidden = true;
         } else {
             parrafo.className = "estilo2";
             colorAlternado = true;
         }
 
-
-
         contenedor.appendChild(parrafo);
-    }
+    });
+
+    /* 
+        let colorAlternado = false;
+        for (let index = 0; index < textosArray.length; index++) {
+            let parrafo = document.createElement('p');
+    
+            parrafo.innerText = textosArray[index];
+            if (colorAlternado == true) {
+                parrafo.className = "estilo1";
+                colorAlternado = false;
+                // parrafo.hidden = true;
+            } else {
+                parrafo.className = "estilo2";
+                colorAlternado = true;
+            }
+    
+    
+    
+            contenedor.appendChild(parrafo);
+        } */
 
     //contenedor.innerText = "Hola";
 
 
+}
+
+
+function obtenerValue() {
+    let ciudades = document.getElementById("ciudades").value;
+
+    if (ciudades == "no_ciudad") {
+        alert("Selecciones Ciudad");
+        return;
+    }
+    alert(ciudades)
+}
+
+generarSelect();
+let ciudadesArray = [
+    "Nueva York",
+    "Los Ángeles",
+    "Chicago",
+    "Houston",
+    "Phoenix",
+    "Filadelfia",
+    "San Antonio",
+    "San Diego",
+    "Dallas",
+    "San José"
+  ];
+function generarSelect() {
+    let ciudades2 = document.getElementById("ciudades2");
+    let opcion = document.createElement("option");
+    let opcion2 = document.createElement("option");
+
+    ciudades2.classList.add("estilo1")
+    ciudades2.classList.add("estilo3")
+
+    opcion.value="villa_maria";
+    opcion.text= "Villa María";
+
+    opcion2.value="villa_nueva";
+    opcion2.text= "Villa Nueva";
+    opcion2.selected = true;
+
+    ciudades2.appendChild(opcion);
+    ciudades2.appendChild(opcion2);
 }
